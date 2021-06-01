2b81d867559730c56c93b16991d09ff79c78c621
theamayen <72680199+theamayen@users.noreply.github.com>
2021-06-01 11:17:39 +0800

    Revert "Login and Registration"
    This reverts commit a7767579d3de2e224be0848e603bc711b8f1573e.

--- MyApplication2/app/src/main/java/com/example/myapplication1/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -13,7 +13,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     EditText username, password;
-    Button login, register, forgot_pw;
+    Button login;
     DatabaseHelper DB;
 
     @Override
@@ -21,11 +21,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        username = (EditText) findViewById(R.id.txt_username);
+        username = (EditText) findViewById(R.id.txt_email);
         password = (EditText) findViewById(R.id.txt_pw);
         login = (Button) findViewById(R.id.button_login);
-        register = (Button) findViewById(R.id.btn_reg);
-        forgot_pw = (Button) findViewById(R.id.btn_forg);
         DB =new DatabaseHelper(this);
 
         login.setOnClickListener(new View.OnClickListener() {
@@ -40,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
                     Boolean checkuserpw = DB.checkusernamepassword(user, pw);
                     if (checkuserpw){
                         Toast.makeText(MainActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(getApplicationContext(), Homepage.class);
+                        Intent intent = new Intent(getApplicationContext(), Home.class);
                         startActivity(intent);
                     }
                     else {
@@ -49,23 +47,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), RegistrationPage.class);
-                startActivity(intent);
-            }
-        });
-
-        forgot_pw.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-
 
     }
 }
