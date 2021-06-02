aed3fa042737f415553d2cb494b0ba8833287a27
svdelera <smnthadelera@gmail.com>
2021-06-02 13:00:24 +0800

    Revert "Revert "Merge branch 'main' of https://github.com/Incognito-Five/HWRU into main""
    This reverts commit 921f17fa7f2d37414412c4a0c86098d2ff65057d.

--- MyApplication2/app/src/main/java/com/example/myapplication1/DatabaseHelper.java
+++ MyApplication2/app/src/main/java/com/example/myapplication1/DatabaseHelper.java
@@ -5,60 +5,58 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-
-    public static final String DBNAME ="Login.db";
-
     public DatabaseHelper(Context context) {
         super(context, "Login.db", null, 1);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase MyDB) {
-        //creates table for users
-        MyDB.execSQL("create Table users(username TEXT primary key, password TEXT)");
+    public void onCreate(SQLiteDatabase DBase) {
+        DBase.execSQL("create Table users(username Text primary key, password Text)");
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase MyDB, int oldVersion, int newVersion) {
-        //user's existence
-        MyDB.execSQL("drop Table if exists users");
+    public void onUpgrade(SQLiteDatabase DBase, int oldVersion, int newVersion) {
+        DBase.execSQL("drop Table if exists users");
     }
 
-    //insertion of username and password
     public Boolean insertData(String username, String password){
-        SQLiteDatabase MyDB = this.getWritableDatabase();
-        ContentValues contentValues =new ContentValues();
-        contentValues.put("username", username);
-        contentValues.put("password", password);
-        long result =MyDB.insert("users", null, contentValues);
-        //insertion not possible
-        if (result == -1)
+        SQLiteDatabase DBase = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("Username", username);
+        contentValues.put("Password", password);
+        long result = DBase.insert("users", null, contentValues);
+        if(result == -1){
             return false;
-        else
+        }
+        else {
             return true;
+        }
     }
 
-    //check if username exists
-    public Boolean checkusername(String username){
-        SQLiteDatabase MyDB = this.getWritableDatabase();
-        Cursor cursor = MyDB.rawQuery("Select * from users where username = ?", new String[] {username});
-        if (cursor.getCount()>0)
-            return  true;
-        else
+    public Boolean checkUsername(String username){
+        SQLiteDatabase DBase = this.getWritableDatabase();
+        Cursor cursor = DBase.rawQuery("select * from users where username = ?", new String[] {username});
+        if (cursor.getCount() > 0){
+            return true;
+        }
+        else {
             return false;
+        }
     }
 
-    //check if username and password exists
-    public Boolean checkusernamepassword(String username, String password){
-        SQLiteDatabase MyDB = this.getWritableDatabase();
-        Cursor cursor = MyDB.rawQuery("Select * from users where username = ? and password = ?", new String[] {username, password});
-        if (cursor.getCount()>0)
-            return  true;
-        else
+    public Boolean checkUsernamePassword(String username, String password){
+        SQLiteDatabase DBase = this.getWritableDatabase();
+        Cursor cursor = DBase.rawQuery("select * from users where username = ? and password = ?", new String[] {username, password});
+        if (cursor.getCount() > 0){
+            return true;
+        }
+        else {
             return false;
+        }
     }
 }
