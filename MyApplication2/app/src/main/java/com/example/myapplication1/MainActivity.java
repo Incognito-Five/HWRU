8f6bf77dc7f99c54a5db92df3314c80f626b6f17
svdelera <smnthadelera@gmail.com>
2021-06-10 18:02:55 +0800

    themes

--- MyApplication2/app/src/main/java/com/example/myapplication1/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.myapplication1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -67,6 +70,22 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        SharedPreferences sharedPreferences = getSharedPreferences("sharedPrefs", MODE_PRIVATE);
+        final boolean isDarkModeOn = sharedPreferences.getBoolean("isDarkModeOn", false);
+
+        // When user reopens the app
+        // after applying dark/light mode
+
+        if (isDarkModeOn){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+
+        }
+        else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+
 
     }
 }
--- MyApplication2/app/src/main/java/com/example/myapplication1/ThemesFragment.java
+++ MyApplication2/app/src/main/java/com/example/myapplication1/ThemesFragment.java
@@ -1,20 +1,74 @@
 package com.example.myapplication1;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
+
+import java.util.Objects;
 
 
 public class ThemesFragment extends Fragment {
 
-    @Nullable
+    Button btn_themes;
+
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_themes, container, false);
+        View view = inflater.inflate(R.layout.fragment_themes, container, false);
+        btn_themes = (Button) view.findViewById(R.id.btn_themes);
+
+        btn_themes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }
+        });
+
+        SharedPreferences sharedPreferences = requireActivity().getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE);
+        final SharedPreferences.Editor editor = sharedPreferences.edit();
+        final boolean isDarkModeOn = sharedPreferences.getBoolean("isDarkModeOn", false);
+
+        // When user reopens the app
+        // after applying dark/light mode
+
+        if (isDarkModeOn){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            btn_themes.setText("Disable");
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            btn_themes.setText("Enable");
+        }
+
+        btn_themes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // When user taps the enable/disable
+                // dark mode button
+                if (isDarkModeOn){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    editor.putBoolean("isDarkModeOn", false);
+                    editor.apply();
+                    btn_themes.setText("Enable");
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    editor.putBoolean("isDarkModeOn", true);
+                    editor.apply();
+                    btn_themes.setText("Disable");
+                }
+            }
+        });
+
+
+        return view;
     }
 }
