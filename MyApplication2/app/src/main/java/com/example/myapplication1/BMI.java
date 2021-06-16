8543d3c8c972873d6d30f05c97818fa71b5732fd
lancey fem <72723309+lanceyfem@users.noreply.github.com>
2021-06-16 17:20:14 +0800

    bmi activity
    bmi activity

--- MyApplication2/app/src/main/java/com/example/myapplication1/BMI.java
+++ MyApplication2/app/src/main/java/com/example/myapplication1/BMI.java
@@ -3,6 +3,10 @@ package com.example.myapplication1;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -15,6 +19,21 @@ import com.google.android.material.navigation.NavigationView;
 
 public class BMI extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
+    /*calculate BMI*/
+    android.widget.Button mcalculatebmi;
+
+    /*BMI Components*/
+    TextView mcurrentheight;
+    TextView mcurrentweight;
+    SeekBar mheightseekbar;
+    SeekBar mweightseekbar;
+
+    /*initialize BMI Component's Values*/
+    int currentheightprogress;
+    int currentweightprogress;
+    String mintheightprogress="180";
+    String mintweightprogress="40";
+
     Toolbar toolbar;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
@@ -23,6 +42,87 @@ public class BMI extends AppCompatActivity implements NavigationView.OnNavigatio
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi);
+        getSupportActionBar().hide();
+        /*defining mcalculatebmi*/
+        mcalculatebmi=findViewById(R.id.calculatebmi);
+        /*assign xml id to java*/
+        mcurrentheight=findViewById(R.id.currentheight);
+        mcurrentweight=findViewById(R.id.currentweight);
+        /*set id for seekbar*/
+        mheightseekbar=findViewById(R.id.heightseekbar);
+        mweightseekbar=findViewById(R.id.weightseekbar);
+
+        mheightseekbar.setMax(400);
+        mheightseekbar.setProgress(180);
+        mheightseekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                currentheightprogress=progress;
+                mintheightprogress=String.valueOf(currentheightprogress);
+                mcurrentheight.setText(mintheightprogress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        mweightseekbar.setMax(650);
+        mweightseekbar.setProgress(40);
+        mweightseekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                currentweightprogress=progress;
+                mintweightprogress=String.valueOf(currentweightprogress);
+                mcurrentweight.setText(mintweightprogress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+
+        mcalculatebmi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*calculating BMI*/
+                if(mintheightprogress.equals("0"))
+                {
+                    Toast.makeText(getApplicationContext(), "Select A Value For Your Height First",Toast.LENGTH_SHORT).show();
+                }
+                else if(mintweightprogress.equals("0"))
+                {
+                    Toast.makeText(getApplicationContext(), "Select A Value For Your Weight First",Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    /*changing screen from main BMI screen to calculate BMI screen*/
+                    Intent intent=new Intent(BMI.this,bmicalculation.class);
+                    /*pass data to calculate BMI*/
+                    intent.putExtra("height",mintheightprogress);
+                    intent.putExtra("weight",mintweightprogress);
+
+
+                    startActivity(intent);
+                }
+
+            }
+        });
+
         toolbar = findViewById(R.id.tb_bmi);
         setSupportActionBar(toolbar);
 
