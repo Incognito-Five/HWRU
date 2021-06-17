6ec6096a107d2789519ae5b0db15ecce8df34793
theamayen <72680199+theamayen@users.noreply.github.com>
2021-06-17 23:33:33 +0800

    Sounds Fragment
    created sounds settings

---
+++ MyApplication2/app/src/main/java/com/example/myapplication1/AlarmSoundsList.java
@@ -0,0 +1,20 @@
+package com.example.myapplication1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class AlarmSoundsList extends AppCompatActivity {
+
+    ListView soundsList;
+    String sounds;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_sounds_list2);
+
+
+    }
+}
--- MyApplication2/app/src/main/java/com/example/myapplication1/SoundsFragment.java
+++ MyApplication2/app/src/main/java/com/example/myapplication1/SoundsFragment.java
@@ -1,11 +1,14 @@
 package com.example.myapplication1;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -13,10 +16,26 @@ import androidx.fragment.app.Fragment;
 
 public class SoundsFragment extends Fragment {
 
+    SeekBar volume_bar;
+    Button alarm_sounds;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sounds, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_sounds, container, false);
+
+        volume_bar = (SeekBar) rootView.findViewById(R.id.volume);
+        alarm_sounds = (Button) rootView.findViewById(R.id.alarm_sounds);
+
+        alarm_sounds.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SoundsFragment.this.getActivity(), AlarmSoundsList.class);
+                startActivity(intent);
+            }
+        });
+
+        return rootView;
     }
 }
