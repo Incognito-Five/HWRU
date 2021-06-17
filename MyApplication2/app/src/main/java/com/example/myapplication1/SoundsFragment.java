8c77f4b02bbfde46a111203fab33bd6d802feb5a
theamayen <72680199+theamayen@users.noreply.github.com>
2021-06-17 16:47:53 +0800

    sounds

--- MyApplication2/app/src/main/java/com/example/myapplication1/SoundsFragment.java
+++ MyApplication2/app/src/main/java/com/example/myapplication1/SoundsFragment.java
@@ -1,9 +1,11 @@
 package com.example.myapplication1;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -11,10 +13,18 @@ import androidx.fragment.app.Fragment;
 
 public class SoundsFragment extends Fragment {
 
+    ImageView sounds;
+    MediaPlayer mp;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sounds, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_sounds, container, false);
+
+        sounds = (ImageView) rootView.findViewById(R.id.sound_icon);
+        mp = MediaPlayer.create(SoundsFragment.this.getActivity(), R.raw.alarm);
+        mp.start();
+        return rootView;
     }
 }
