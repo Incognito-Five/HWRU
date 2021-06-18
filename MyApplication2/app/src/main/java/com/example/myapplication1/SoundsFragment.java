8a6afd72169b2e0d65924f636ec143cfaa1cb2fa
theamayen <72680199+theamayen@users.noreply.github.com>
2021-06-18 13:40:39 +0800

    SoundSetting
    Started sound setting with sample music

--- MyApplication2/app/src/main/java/com/example/myapplication1/SoundsFragment.java
+++ MyApplication2/app/src/main/java/com/example/myapplication1/SoundsFragment.java
@@ -1,6 +1,8 @@
 package com.example.myapplication1;
 
+import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,14 +12,17 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 
+import androidx.annotation.AnimatorRes;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 
 public class SoundsFragment extends Fragment {
 
-    SeekBar volume_bar;
-    Button alarm_sounds;
+    SeekBar notif_bar, alarm_bar;
+    Button play, pause;
+    MediaPlayer mediaPlayer;
+    AudioManager audioManager;
 
     @Nullable
     @Override
@@ -25,14 +30,52 @@ public class SoundsFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_sounds, container, false);
 
-        volume_bar = (SeekBar) rootView.findViewById(R.id.volume);
-        alarm_sounds = (Button) rootView.findViewById(R.id.alarm_sounds);
+        mediaPlayer = MediaPlayer.create(SoundsFragment.this.getActivity(), R.raw.alarm);
+        audioManager = (AudioManager) SoundsFragment.this.getActivity().getSystemService(Context.AUDIO_SERVICE);
 
-        alarm_sounds.setOnClickListener(new View.OnClickListener() {
+        //volumes
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int currVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        //seekbar
+        notif_bar = (SeekBar) rootView.findViewById(R.id.notif_volume);
+
+        //alarm seekbar
+        alarm_bar = (SeekBar) rootView.findViewById(R.id.alarm_volume);
+        alarm_bar.setMax(maxVolume);
+        alarm_bar.setProgress(currVolume);
+
+        alarm_bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        //buttons
+        play = (Button) rootView.findViewById(R.id.play_btn);
+        pause = (Button) rootView.findViewById(R.id.pause_btn);
+
+        play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SoundsFragment.this.getActivity(), AlarmSoundsList.class);
-                startActivity(intent);
+                mediaPlayer.start();
+            }
+        });
+
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.pause();
             }
         });
 
