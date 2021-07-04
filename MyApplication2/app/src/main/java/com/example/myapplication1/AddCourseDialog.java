f4cf995c7449bef0cf8e7ae47e88283a9bf27487
svdelera <smnthadelera@gmail.com>
2021-07-04 18:55:11 +0800

    add_course_dialog

---
+++ MyApplication2/app/src/main/java/com/example/myapplication1/AddCourseDialog.java
@@ -0,0 +1,104 @@
+package com.example.myapplication1;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TimePicker;
+import android.widget.Toolbar;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class AddCourseDialog extends DialogFragment {
+
+    Button save_btn, cancel_btn;
+    TextInputEditText pick_start_time, pick_end_time;
+    TimePickerDialog timePickerDialog;
+    Calendar calendar;
+    int currentHour, currentMin, currentHour2, currentMin2;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable  Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dialog_addcourse, container, false);
+
+        save_btn = view.findViewById(R.id.save_btn);
+        cancel_btn = view.findViewById(R.id.cancel_btn);
+
+        pick_start_time = view.findViewById(R.id.pick_start_time);
+        pick_end_time = view.findViewById(R.id.pick_end_time);
+
+        cancel_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+        pick_start_time.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                calendar = Calendar.getInstance();
+                currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+                currentMin = calendar.get(Calendar.MINUTE);
+
+                timePickerDialog = new TimePickerDialog(getActivity(), new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        String time = hourOfDay + ":" + minute ;
+                        SimpleDateFormat f24Hours = new SimpleDateFormat("HH:mm");
+                        try {
+                            Date date = f24Hours.parse(time);
+                            SimpleDateFormat f12Hours = new SimpleDateFormat("hh:mm aa");
+                            pick_start_time.setText(f12Hours.format(date));
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },12,0,false);
+                timePickerDialog.show();
+            }
+        });
+
+        pick_end_time.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                calendar = Calendar.getInstance();
+                currentHour2 = calendar.get(Calendar.HOUR_OF_DAY);
+                currentMin2 = calendar.get(Calendar.MINUTE);
+
+                timePickerDialog = new TimePickerDialog(getActivity(), new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        String time = hourOfDay + ":" + minute ;
+                        SimpleDateFormat f24Hours = new SimpleDateFormat("HH:mm");
+                        try {
+                            Date date = f24Hours.parse(time);
+                            SimpleDateFormat f12Hours = new SimpleDateFormat("hh:mm aa");
+                            pick_end_time.setText(f12Hours.format(date));
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },12,0,false);
+                timePickerDialog.show();
+            }
+        });
+        return view;
+    }
+
+}
