afe8c3dc1d5361728e91e772710a2c54f4296707
svdelera <smnthadelera@gmail.com>
2021-07-05 21:55:15 +0800

    Course input to dbase

--- MyApplication2/app/src/main/java/com/example/myapplication1/DialogAddCourse.java
+++ MyApplication2/app/src/main/java/com/example/myapplication1/DialogAddCourse.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -22,10 +23,11 @@ import java.util.Date;
 public class DialogAddCourse extends DialogFragment {
 
     Button save_btn, cancel_btn;
-    TextInputEditText pick_start_time, pick_end_time;
+    TextInputEditText start_time, end_time, course_name, course_code, professor, description;
     TimePickerDialog timePickerDialog;
     Calendar calendar;
     int currentHour, currentMin, currentHour2, currentMin2;
+    TimetableDBHelper DB;
 
     @Nullable
     @Override
@@ -35,8 +37,35 @@ public class DialogAddCourse extends DialogFragment {
         save_btn = view.findViewById(R.id.save_btn);
         cancel_btn = view.findViewById(R.id.cancel_btn);
 
-        pick_start_time = view.findViewById(R.id.pick_start_time);
-        pick_end_time = view.findViewById(R.id.pick_end_time);
+        start_time = view.findViewById(R.id.pick_start_time);
+        end_time = view.findViewById(R.id.pick_end_time);
+        course_name = view.findViewById(R.id.input_course_name);
+        course_code = view.findViewById(R.id.input_course_code);
+        professor = view.findViewById(R.id.input_professor);
+        description = view.findViewById(R.id.input_description);
+
+        DB = new TimetableDBHelper(getActivity());
+
+        save_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String course_nameTXT = course_name.getText().toString();
+                String course_codeTXT = course_code.getText().toString();
+                String professorTXT = professor.getText().toString();
+                String start_timeTXT = start_time.getText().toString();
+                String end_timeTXT = end_time.getText().toString();
+                String descriptionTXT = description.getText().toString();
+
+                Boolean checkinsertdata = DB.AddCourseData(course_nameTXT, course_codeTXT,professorTXT, start_timeTXT,end_timeTXT,descriptionTXT);
+                if(checkinsertdata){
+                    Toast.makeText(getActivity(),"New Course Added", Toast.LENGTH_SHORT).show();
+                    dismiss();
+                } else {
+                    Toast.makeText(getActivity(),"Course not added.Try Again.", Toast.LENGTH_SHORT).show();
+                    dismiss();
+                }
+            }
+        });
 
         cancel_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,7 +74,7 @@ public class DialogAddCourse extends DialogFragment {
             }
         });
 
-        pick_start_time.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+        start_time.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
                 calendar = Calendar.getInstance();
@@ -60,7 +89,7 @@ public class DialogAddCourse extends DialogFragment {
                         try {
                             Date date = f24Hours.parse(time);
                             SimpleDateFormat f12Hours = new SimpleDateFormat("hh:mm aa");
-                            pick_start_time.setText(f12Hours.format(date));
+                            start_time.setText(f12Hours.format(date));
                         } catch (ParseException e) {
                             e.printStackTrace();
                         }
@@ -70,7 +99,7 @@ public class DialogAddCourse extends DialogFragment {
             }
         });
 
-        pick_end_time.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+        end_time.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
                 calendar = Calendar.getInstance();
@@ -85,7 +114,7 @@ public class DialogAddCourse extends DialogFragment {
                         try {
                             Date date = f24Hours.parse(time);
                             SimpleDateFormat f12Hours = new SimpleDateFormat("hh:mm aa");
-                            pick_end_time.setText(f12Hours.format(date));
+                            end_time.setText(f12Hours.format(date));
                         } catch (ParseException e) {
                             e.printStackTrace();
                         }
