d039f28273dd3264ff177c67f53f09104802d672
lancey fem <72723309+lanceyfem@users.noreply.github.com>
2021-07-14 22:13:14 +0800

    course layout

---
+++ MyApplication2/app/src/main/java/com/example/myapplication1/Timetable/CourseListMain.java
@@ -0,0 +1,17 @@
+package com.example.myapplication1.Timetable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication1.R;
+
+public class CourseListMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_list_main);
+        
+    }
+}
---
+++ MyApplication2/app/src/main/java/com/example/myapplication1/Timetable/CourseUpdate.java
@@ -0,0 +1,14 @@
+package com.example.myapplication1.Timetable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CourseUpdate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_update);
+    }
+}
