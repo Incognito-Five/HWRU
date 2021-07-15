c7380c1b0edc7f87e355e072374c7e11dc01d998
svdelera <smnthadelera@gmail.com>
2021-07-15 20:49:49 +0800

    .

--- MyApplication2/app/src/main/java/com/example/myapplication1/Timetable/ReminderBroadcast.java
+++ MyApplication2/app/src/main/java/com/example/myapplication1/Timetable/ReminderBroadcast.java
@@ -14,40 +14,5 @@ public class ReminderBroadcast extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-        Intent intent1 = new Intent(context, ScheduleReminder.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent1, 0);
-=======
-/*       Intent intent1 = new Intent(context, ScheduleReminder.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent1, 0);*//*
->>>>>>> parent of 6f3bcfb (Revert "Merge branch 'main' of https://github.com/Incognito-Five/HWRU into main")
-=======
-/*       Intent intent1 = new Intent(context, ScheduleReminder.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent1, 0);*//*
->>>>>>> parent of 6f3bcfb (Revert "Merge branch 'main' of https://github.com/Incognito-Five/HWRU into main")
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "notifyuser")
-                .setSmallIcon(R.id.alarm_icon)
-                .setContentTitle("Schedule Reminder")
-                .setContentText("Hey student, this is a soft reminder of your today's schedule...")
-                .setAutoCancel(true)
-                .setDefaults(NotificationCompat.DEFAULT_ALL)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setContentIntent(pendingIntent);
-
-        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
-<<<<<<< HEAD
-<<<<<<< HEAD
-        notificationManagerCompat.notify(200, builder.build());
-=======
-        notificationManagerCompat.notify(200, builder.build());*/
->>>>>>> parent of 6f3bcfb (Revert "Merge branch 'main' of https://github.com/Incognito-Five/HWRU into main")
-=======
-        notificationManagerCompat.notify(200, builder.build());*/
->>>>>>> parent of 6f3bcfb (Revert "Merge branch 'main' of https://github.com/Incognito-Five/HWRU into main")
-    }
+   }
 }
