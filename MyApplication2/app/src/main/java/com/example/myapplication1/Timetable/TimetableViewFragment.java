6f3bcfb3fedd3015cbdadddb0e0b5c9b5215beb9
lancey fem <72723309+lanceyfem@users.noreply.github.com>
2021-07-15 11:17:44 +0800

    Revert "Merge branch 'main' of https://github.com/Incognito-Five/HWRU into main"
    This reverts commit 5e260274f7b8166933cbc61a2d14cede6a242bb4, reversing
    changes made to aca0934622867f9c9a69b62fc4508f670c31f757.

--- MyApplication2/app/src/main/java/com/example/myapplication1/Timetable/TimetableViewFragment.java
+++ MyApplication2/app/src/main/java/com/example/myapplication1/Timetable/TimetableViewFragment.java
@@ -9,26 +9,27 @@ import androidx.viewpager2.widget.ViewPager2;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.example.myapplication1.R;
 import com.google.android.material.tabs.TabLayout;
 
-public class ScheduleFragment extends Fragment {
+public class TimetableViewFragment extends Fragment {
 
     TabLayout tab_week;
     ViewPager2 vp;
-    WeekTabAdapter adapter2;
+    FragmentAdapter2 adapter2;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_schedule, container, false);
+        View view = inflater.inflate(R.layout.fragment_timetable_view, container, false);
 
         tab_week = view.findViewById(R.id.tab_week);
         vp = view.findViewById(R.id.vp);
 
         FragmentManager fm = getActivity().getSupportFragmentManager();
-        adapter2 = new WeekTabAdapter(fm,getLifecycle());
+        adapter2 = new FragmentAdapter2(fm,getLifecycle());
         vp.setAdapter(adapter2);
 
         tab_week.addTab(tab_week.newTab().setText("Monday"));
