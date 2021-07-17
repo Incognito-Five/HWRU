be813fc8c01629627d151143a13f6651819b0f09
taemagura <72679657+taemagura@users.noreply.github.com>
2021-07-18 00:52:49 +0800

    Splash screen (hwru logo)

---
+++ MyApplication2/app/src/main/java/com/example/myapplication1/SplashScreen.java
@@ -0,0 +1,21 @@
+package com.example.myapplication1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+        finish();
+
+    }
+}
